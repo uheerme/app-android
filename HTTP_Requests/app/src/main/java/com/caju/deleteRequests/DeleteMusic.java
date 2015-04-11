089b5f987427ad05e6e627608060d2d89b044521
Camilo Moreira <camilomoreira91@gmail.com>
2015-04-11 14:10:00 -0300

    Added new HTTP Requests packages and Classes

---
+++ HTTP_Requests/app/src/main/java/com/caju/deleteRequests/DeleteMusic.java
@@ -0,0 +1,9 @@
+package com.caju.deleteRequests;
+
+/**
+ * Created by camilo on 11/04/15.
+ */
+public class DeleteMusic
+{
+    
+}
---
+++ HTTP_Requests/app/src/main/java/com/caju/putRequests/PutChannel.java
@@ -0,0 +1,9 @@
+package com.caju.putRequests;
+
+/**
+ * Created by camilo on 11/04/15.
+ */
+public class PutChannel
+{
+
+}
