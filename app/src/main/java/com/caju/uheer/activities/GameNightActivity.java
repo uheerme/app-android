00112a5d9bc472ba41596c02aeb457a2c153d64c
João Paulo <smokeonline@gmail.com>
2015-05-07 11:39:07 -0300

    GetActiveChannels class is returning a instance of Channel now. Updated buttons in GameNightActivity.

--- app/src/main/java/com/caju/uheer/activities/GameNightActivity.java
+++ app/src/main/java/com/caju/uheer/activities/GameNightActivity.java
@@ -1,20 +1,22 @@
 package com.caju.uheer.activities;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Button;
 
 import com.caju.uheer.R;
+import com.caju.uheer.core.Channel;
+import com.caju.uheer.infrastructure.interfaces.OnFailedListener;
+import com.caju.uheer.infrastructure.interfaces.OnFinishedListener;
 import com.caju.utils.exceptions.NoConnectionException;
 import com.caju.utils.exceptions.NoIDSelectedException;
 import com.caju.utils.getRequests.GetActiveChannels;
 import com.caju.utils.getRequests.GetStatusNow;
-import com.caju.uheer.infrastructure.interfaces.OnFailedListener;
-import com.caju.uheer.infrastructure.interfaces.OnFinishedListener;
 
-import org.json.JSONArray;
-import org.json.JSONException;
+import java.util.List;
 
 /**
  * Created by Jp on 06/05/2015.
@@ -23,6 +25,8 @@ public class GameNightActivity extends ActionBarActivity implements OnFinishedLi
     private int request;
     private GetActiveChannels getActiveChannels;
     private GetStatusNow getStatusNow;
+    private Button firstButtonChannel;
+    private Button secondButtonChannel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +50,9 @@ public class GameNightActivity extends ActionBarActivity implements OnFinishedLi
         {
             Log.e("onCreate getChannel", "You have no ID.");
         }
+
+        firstButtonChannel = (Button)findViewById(R.id.channel1);
+        secondButtonChannel = (Button)findViewById(R.id.channel2);
     }
 
     @Override
@@ -63,18 +70,14 @@ public class GameNightActivity extends ActionBarActivity implements OnFinishedLi
     @Override
     public void onLoadFinished() {
         if(request == 1){
-            if(getActiveChannels.getResultResponse() != null) {
-                JSONArray activeChannels = getActiveChannels.getResultResponse();
-                try {
-                    Log.d("onLoadFinished1", activeChannels.getJSONObject(activeChannels.length()-1).getString("Name"));
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-                try {
-                    Log.d("onLoadFinished2", activeChannels.getJSONObject(activeChannels.length()-2).getString("Name"));
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
+            List<Channel> activeChannels;
+            if((activeChannels = getActiveChannels.getResultResponse()) != null) {
+                Log.d("onLoadFinished1", activeChannels.get(0).Name.toString());
+                Log.d("onLoadFinished2", activeChannels.get(1).Name.toString());
+                firstButtonChannel.setText(activeChannels.get(activeChannels.size() - 1).Name);
+                secondButtonChannel.setText(activeChannels.get(activeChannels.size()-2).Name);
+                firstButtonChannel.setBackgroundColor(Color.parseColor("#ffffc8"));
+                secondButtonChannel.setBackgroundColor(Color.parseColor("#a0daef"));
             }
             else
                 Log.e("onLoadFinished", "This shouldn't happen");
