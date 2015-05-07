3555983258629de3c2384f1126fb5e39d8a6d70a
Lucas David <lucas.david@drexel.edu>
2015-05-06 22:24:22 -0300

    Add core classes

---
+++ app/src/main/java/com/caju/uheer/core/Channel.java
@@ -0,0 +1,25 @@
+package com.caju.uheer.core;
+
+import java.util.Date;
+import java.util.List;
+
+public class Channel {
+
+    public int Id;
+    public String Name;
+
+    public String Owner;
+    public String HostIpAddress;
+    public String HostMacAddress;
+
+    public boolean Loops;
+
+    public int CurrentId;
+    public Date CurrentStartTime;
+
+    public List<Music> Musics;
+
+    public Date DateCreated;
+    public Date DateUpdated;
+    public Date DateDeactivated;
+}
---
+++ app/src/main/java/com/caju/uheer/core/Music.java
@@ -0,0 +1,14 @@
+package com.caju.uheer.core;
+
+import java.util.Date;
+
+public class Music {
+    public int Id;
+
+    public String Name;
+    public int SizeInBytes;
+    public int LengthInMilliseconds;
+
+    public Date DateCreated;
+    public Date DateUpdated;
+}
