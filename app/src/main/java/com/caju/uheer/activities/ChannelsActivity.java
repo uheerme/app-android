607b5e946ee6b6fc29f422ddf04edbf84fb7e56a
Lucas David <lucas.david@drexel.edu>
2015-05-07 22:09:02 -0300

    Update ChannelsActivity
    We now request the channels asynchronously with spring's help!

--- app/src/main/java/com/caju/uheer/activities/ChannelsActivity.java
+++ app/src/main/java/com/caju/uheer/activities/ChannelsActivity.java
@@ -1,39 +1,67 @@
 package com.caju.uheer.activities;
 
-import android.support.v7.app.ActionBarActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.caju.uheer.R;
+import com.caju.uheer.core.Channel;
+import com.caju.uheer.infrastructure.interfaces.Routes;
 
-public class ChannelsActivity extends ActionBarActivity {
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+public class ChannelsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_channels);
+
+        new AllChannelsTask().execute();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_channels, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class AllChannelsTask extends AsyncTask<Void, Void, Channel[]> {
+        @Override
+        protected Channel[] doInBackground(Void... params) {
+            Channel[] channels = null;
+
+            try {
+                RestTemplate restTemplate = new RestTemplate();
+                restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+
+                channels = restTemplate.getForObject(Routes.CHANNELS, Channel[].class);
+            } catch (Exception e) {
+                Log.e("ChannelsActivity", e.getMessage(), e);
+            }
+
+            return channels;
+        }
+
+        @Override
+        protected void onPostExecute(Channel[] channels) {
+            for (Channel channel : channels) {
+                Log.d("ChannelsActivity", channel.Name);
+            }
+        }
     }
 }
