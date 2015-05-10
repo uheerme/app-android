0df0ac2a49ad1a8c4552de253673eaa692fe8545
Lucas David <lucas.david@drexel.edu>
2015-05-09 23:29:11 -0300

    Add missing package interfaces

---
+++ app/src/main/java/com/caju/uheer/interfaces/Routes.java
@@ -0,0 +1,10 @@
+package com.caju.uheer.interfaces;
+
+public interface Routes {
+
+    String URL = "http://uheer.me/api/";
+
+    String CHANNELS = URL + "channels/";
+    String MUSICS   = URL + "musics/";
+    String STATUS   = URL + "status/";
+}
