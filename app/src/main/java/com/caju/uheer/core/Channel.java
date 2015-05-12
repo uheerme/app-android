96425b3ca2848e4bcd3db6b9d57acdbd7e538ef2
Lucas David <lucas.david@drexel.edu>
2015-05-12 19:42:05 -0300

    Update core classes

--- app/src/main/java/com/caju/uheer/core/Channel.java
+++ app/src/main/java/com/caju/uheer/core/Channel.java
@@ -1,8 +1,13 @@
 package com.caju.uheer.core;
 
+import com.caju.uheer.services.exceptions.EndOfPlaylistException;
+import com.caju.uheer.services.exceptions.NextMusicDoesNotExistException;
+
 import java.util.Date;
 
 public class Channel {
+
+    private int currentIndex;
 
     public int Id;
     public String Name;
@@ -14,14 +19,63 @@ public class Channel {
     public boolean Loops;
 
     public int CurrentId;
+    public Music current;
     public Date CurrentStartTime;
 
     public Music[] Musics;
+    public int LengthInMilliseconds;
 
     public Date DateCreated;
     public Date DateUpdated;
     public Date DateDeactivated;
 
+    public Channel analyzePlaylist() {
+        if (CurrentId == 0) {
+            currentIndex = -1;
+            current = null;
+
+            return this;
+        }
+
+        LengthInMilliseconds = 0;
+
+        for (int i = 0; i < Musics.length; i++) {
+            Music music = Musics[i];
+
+            LengthInMilliseconds += music.LengthInMilliseconds;
+
+            if (music.Id == CurrentId) {
+                currentIndex = i;
+                current = music;
+            }
+        }
+
+        return this;
+    }
+
+    public Music next() {
+        if (currentIndex == -1) {
+            throw new NextMusicDoesNotExistException();
+        }
+
+        int nextIndex = currentIndex + 1;
+
+        if (nextIndex + 1 == Musics.length && !Loops) {
+            currentIndex = -1;
+            CurrentId = -1;
+            current = null;
+            throw new EndOfPlaylistException();
+        }
+
+        currentIndex = nextIndex % Musics.length;
+        current = Musics[currentIndex];
+        CurrentId = current.Id;
+
+        return current;
+    }
+
     @Override
-    public String toString(){ return this.Name;}
+    public String toString() {
+        return "#" + Id + " " + this.Name;
+    }
 }
