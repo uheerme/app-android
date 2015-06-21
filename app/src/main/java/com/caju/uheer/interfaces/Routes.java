80920c57bff00e0a902ae5ae1fd5282a5610cbce
Lucas David <lucas.david@drexel.edu>
2015-06-21 16:36:58 -0300

    Update Routes
    Use IP to avoid domain translation overcharge.

--- app/src/main/java/com/caju/uheer/interfaces/Routes.java
+++ app/src/main/java/com/caju/uheer/interfaces/Routes.java
@@ -2,7 +2,7 @@ package com.caju.uheer.interfaces;
 
 public interface Routes {
 
-    String URL = "http://uheer.me/api/";
+    String URL = "http://54.207.33.242/api/";
 
     String CHANNELS = URL + "channels/";
     String MUSICS   = URL + "musics/";
