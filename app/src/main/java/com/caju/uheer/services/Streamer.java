7c8bba773b2fdcc1877c99bfb2493927b7fc8ae6
Lucas David <lucas.david@drexel.edu>
2015-06-21 13:11:09 -0300

    Update Streamer
    Rollback HashMap removal. The streamer now maintains its references and checks for the file before streaming.

--- app/src/main/java/com/caju/uheer/services/Streamer.java
+++ app/src/main/java/com/caju/uheer/services/Streamer.java
@@ -16,33 +16,52 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.HashMap;
 
 
 public class Streamer {
 
-    private final Context context;
-    private IStreamingListener listener;
+    protected final Context context;
+    protected IStreamingListener listener;
+
+    protected HashMap<Integer, StreamItem> streams;
 
     public Streamer(Context context) {
         this.context = context;
+        this.streams = new HashMap<>();
     }
 
+    protected String getMusicPath(Music music) {
+        return context.getFilesDir() + "/" + music.Id + music.Name;
+    }
+
+    protected File getMusicFile(Music music) {
+        return new File(getMusicPath((music)));
+    }
+    
     public StreamItem stream(Music music) {
-        Log.d("Stream request", music.toString());
-        Log.d("Stream request", ""+(GlobalVariables.counter++));
-        Log.d("Stream request", "currentThread "+Thread.currentThread().getId()+" - "+Thread.currentThread().getName());
+        Log.d("Stream request", music.toString() + " " + (GlobalVariables.counter++));
 
-        // Tries to get the stream, if it has already started.
-        File file = new File(context.getFilesDir() + "/" + music.Id + music.Name);
-        StreamItem item = new StreamItem(music, file, null);
+        // Tries to get the stream, if it has already started in this uheer instance.
+        StreamItem stream = streams.get(new Integer(music.Id));
 
-        new StreamTask().execute(music);
+        if (stream == null) {
+            Log.d("Streamer", "Stream not found. Trying to stream...");
 
-        return item;
+            // It didn't. Let's try to stream it.
+            stream = new StreamItem(
+                    music,
+                    getMusicFile(music),
+                    new StreamTask().execute(music));
+
+            streams.put(new Integer(music.Id), stream);
+        }
+
+        return stream;
     }
 
     public interface IStreamingListener {
-        void onFinished(StreamItem item);
+        void onFinished();
     }
 
     public Streamer setListener(IStreamingListener listener) {
@@ -50,33 +69,34 @@ public class Streamer {
         return this;
     }
 
-    private class StreamTask extends AsyncTask<Music, Void, StreamItem> {
+    protected class StreamTask extends AsyncTask<Music, Void, StreamItem> {
 
         @Override
         protected StreamItem doInBackground(Music... params) {
             Music music = params[0];
 
             try {
-                // Get file's length
+                // Get file's length. If we already have the audio file stored and its length is the same,
+                // we don't have to stream, as this song was streamed already, in a different uheer instance.
                 URL url = new URL(Routes.MUSICS + music.Id + "/stream");
-                URLConnection conection= url.openConnection();
-                conection.connect();
-                int fileLength = conection.getContentLength();
-                Log.d("fileLength",""+fileLength);
+                URLConnection c = url.openConnection();
+                c.connect();
+                int length = c.getContentLength();
 
-                // Opens stream for the songs URL.
-                InputStream input = new BufferedInputStream(url.openStream());
-
-                // Creates the file.
-                File file = new File(context.getFilesDir() + "/" + music.Id + music.Name);
+                // Creates or retrieves the file.
+                File file = getMusicFile(music);
 
                 // Informs Streamer that we're downloading this file.
                 StreamItem item = new StreamItem(music, file, this);
 
                 // If the file already exists we don't need to download
-                if(file.exists() && file.length() == fileLength)
+                if (file.exists() && file.length() == length) {
+                    Log.d("StreamTask", "An old copy of the song was found. Stream is unnecessary.");
                     return item;
+                }
 
+                // Opens stream for the songs URL.
+                InputStream input = new BufferedInputStream(url.openStream());
                 FileOutputStream output = new FileOutputStream(file);
 
                 // Transfers data in input to output.
@@ -86,8 +106,7 @@ public class Streamer {
 
                 while ((count = input.read(data)) != -1) {
                     downloaded += count;
-                    Log.d("Downloaded progress",""+downloaded*100/fileLength);
-                    GlobalVariables.downloadProgress = downloaded*100/fileLength;
+                    GlobalVariables.downloadProgress = downloaded * 100 / length;
                     output.write(data, 0, count);
                 }
 
@@ -105,9 +124,10 @@ public class Streamer {
 
         @Override
         protected void onPostExecute(StreamItem item) {
-            Log.d("onPostExecute", "Streamer");
+            Log.d("Streamer", "Finished stream of" + item.music.Name);
+
             if (listener != null) {
-                listener.onFinished(item);
+                listener.onFinished();
             }
         }
     }
