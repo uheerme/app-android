fd754e9a95e071a77735387d1ac7488276a599fb
João Paulo <smokeonline@gmail.com>
2015-11-09 09:48:41 -0200

    Getting location data in Friends In Other Places activity.

--- app/src/main/java/com/caju/uheer/activities/FriendsInOtherPlacesActivity.java
+++ app/src/main/java/com/caju/uheer/activities/FriendsInOtherPlacesActivity.java
@@ -1,9 +1,14 @@
 package com.caju.uheer.activities;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.caju.uheer.R;
 
@@ -13,6 +18,33 @@ public class FriendsInOtherPlacesActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends_in_other_places);
+
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                TextView tv = (TextView)findViewById(R.id.gps_data);
+                tv.setText("Location: "+location.toString());
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        // Minimum of 2 minutes between checks (120000 milisecs).
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 120000, 0, locationListener);
     }
 
     @Override
